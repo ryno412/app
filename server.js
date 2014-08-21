b57d2423bf9510099e51c6866fa95461fc61825a
ryno <ryno412@gmail.com>
2014-08-20 22:02:54 -0700

    adding auth

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-
 var express         = require('express'),
     https           = require('https'),
     passport        = require('passport'),
+    LocalStrategy   = require('passport-local').Strategy;
     hbs             = require('hbs'),
     path            = require('path'),
     fs              = require('fs'),
@@ -19,28 +19,24 @@ var express         = require('express'),
     env             = process.env.NODE_ENV || 'development';
     app             = express();
 
-
-
 if (env === 'development') {
     app.set("dbURL","mongodb://localhost:27017/node-mongo-ex");
     app.set('host', 'http://localhost:8080')
 }
+console.log("yoooooo")
 
 if (env === 'production') {
     app.set('host', 'http://ec2-54-200-74-201.us-west-2.compute.amazonaws.com');
-    app.set("dbURL",process.env.MONGOHQ_URL);
+    //app.set("dbURL",process.env.MONGOHQ_URL);
 }
 
     app.set('port', process.env.PORT || 3000);
     app.set('views', __dirname + '/server/templates');
     app.set('view engine', 'html');
     app.engine('html', require('hbs').__express);
-
     app.use(serveStatic(__dirname + '/public', {'index': ['index.html', 'index.htm']}));
-    //app.use(favicon());
     app.use(bodyParser());
     app.use(cookieParser('H3R0'));
-
 
     app.use(session(
         { secret: 'H3R0',
@@ -54,22 +50,56 @@ if (env === 'production') {
     app.use(passport.session());
     app.use(flash());
 
+    passport.use(new LocalStrategy({usernameField: 'email', passwordField: 'password'},
+      function(username, password, done) {
+      console.log('$$$$$$$$$', username)
+      console.log('$$$$$$$$$', password)
+      var user = {id:'ryno',name:'ryan'};
+      return done(null,user);
+/*        User.findOne({ username: username }, function (err, user) {
+          if (err) { return done(err); }
+          if (!user) { return done(null, false); }
+          if (!user.validPassword(password)) { return done(null, false); }
+          return done(null, user);
+        });*/
+      }
+    ));
+    passport.serializeUser(function(user, done) {
+    console.log("S",user)
+        done(null, user.id);
+    });
+
+    passport.deserializeUser(function(userId, done) {
+        console.log("D",userId)
+        done(null,{id:userId})
+    });
+
 /*----Routes----*/
 /* server side routes */
 
 /* client side routes -  just catch everything and let the backbone router take care of it*/
-app.route('/*').get(function (req, res) {
+app.route('/login').get(function (req, res) {
+    res.render('login')
+})
+app.route('/authenticate').post(
+  passport.authenticate('local',
+  { successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true })
+);
+app.route('/*').all(auth(false)).get(function (req, res) {
+console.log("user in client route", req.user)
+console.log("session in client route", req.session)
         var name = (req.user && req.user.name) ? req.user.name : "";
         res.render('index', {layout:false, name:name});
-    });
+});
 
-app.post('/authenticate',
-    passport.authenticate('local', { successRedirect: '/',failureRedirect: '/login',failureFlash: true })
-);
+
 
 
 function auth(ajax){
     return function(req, res, next){
+    console.log("user in auth",req.user)
         if (!req.user){
             ajax ? res.send(401) : res.redirect("/login");
         }
